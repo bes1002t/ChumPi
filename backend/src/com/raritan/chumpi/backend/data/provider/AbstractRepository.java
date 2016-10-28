6bbc21781ac2d0e628b886c70ca6f41f261198a8
feesu <fes10031@fh-zwickau.de>
2016-10-28 23:52:32 +0200

    make repository generic

---
+++ backend/src/com/raritan/chumpi/backend/data/provider/AbstractRepository.java
@@ -0,0 +1,73 @@
+package com.raritan.chumpi.backend.data.provider;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import com.raritan.chumpi.backend.rest.server.GsonCreator;
+
+public abstract class AbstractRepository<T> extends GsonCreator {
+	
+	protected final Set<T> cache = new HashSet<>();
+	private final Type repoType = new TypeToken<T>(){}.getType();
+	private final File dataStoreLocation;
+	private final String fileExtension = ".json";
+	
+	protected AbstractRepository() {
+		createGson();
+		dataStoreLocation = new File("datastore/" + repoType.getTypeName() + "s/");
+	}
+
+	// only for testing
+	public void printContent() {
+		for (T t : cache) {
+			System.out.println(t);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void reloadCache() {
+		System.out.println("Reload user profiles");
+		cache.clear();
+		for (File f : (List<File>) FileUtils.listFiles(dataStoreLocation, 
+				TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE)) {
+			try {
+				T u = getGson().fromJson(FileUtils.readFileToString(f), repoType);
+				cache.add(u);
+				System.out.println("added " + u);
+			} catch (JsonSyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void persistCache() {
+		for (T u : cache) {
+			persist(u);
+		}
+	}
+	
+	protected void persist(T u) {
+		File uFile = new File(dataStoreLocation + "/" + u.toString().replaceAll("\\s", "_") + fileExtension);
+		String uJson = getGson().toJson(u);
+		try {
+			FileUtils.writeStringToFile(uFile, uJson);
+			System.out.println("persist user successfully to " + uFile.getAbsolutePath());
+		} catch (IOException e) {
+			System.err.println("Could not persist user " + u.toString() + " to data store!");
+			e.printStackTrace();
+		}
+	}
+}
--- backend/src/com/raritan/chumpi/backend/data/provider/UserRepository.java
+++ backend/src/com/raritan/chumpi/backend/data/provider/UserRepository.java
@@ -1,79 +1,20 @@
 package com.raritan.chumpi.backend.data.provider;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.TrueFileFilter;
-
-import com.google.gson.JsonSyntaxException;
 import com.raritan.chumpi.backend.data.User;
 import com.raritan.chumpi.backend.exceptions.UserNotFoundException;
-import com.raritan.chumpi.backend.rest.server.GsonCreator;
 
-public class UserRepository extends GsonCreator {
+public class UserRepository extends AbstractRepository<User> {
 	
 	public final static UserRepository INSTANCE = new UserRepository();
 	
-	private final File dataStoreLocation = new File("datastore/users/");
-	private final String userExtension = ".json";
-	private final Set<User> userCache = new HashSet<>();
-	
 	private UserRepository() {
-		createGson();
+		super();
 	}
 	
-	// only for testing
-	public void printContent() {
-		for (User u : userCache) {
-			System.out.println(u);
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void reloadUsers() {
-		System.out.println("Reload user profiles");
-		userCache.clear();
-		for (File f : (List<File>) FileUtils.listFiles(dataStoreLocation, 
-				TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE)) {
-			try {
-				User u = getGson().fromJson(FileUtils.readFileToString(f), User.class);
-				userCache.add(u);
-				System.out.println("added " + u);
-			} catch (JsonSyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public void persistUsers() {
-		for (User u : userCache) {
-			persistUser(u);
-		}
-	}
-	
-	private void persistUser(User u) {
-		File uFile = new File(dataStoreLocation + "/" + u.toString().replaceAll("\\s", "_") + userExtension);
-		String uJson = getGson().toJson(u);
-		try {
-			FileUtils.writeStringToFile(uFile, uJson);
-			System.out.println("persist user successfully to " + uFile.getAbsolutePath());
-		} catch (IOException e) {
-			System.err.println("Could not persist user " + u.toString() + " to data store!");
-			e.printStackTrace();
-		}
-	}
-
 	public User getUserByName(String name) throws UserNotFoundException {
-		for (User u : userCache) {
+		for (User u : cache) {
 			if (u.getName().equals(name))
 				return u;
 		}
@@ -81,7 +22,7 @@ public class UserRepository extends GsonCreator {
 	}
 	
 	public User getUser(int id) throws UserNotFoundException {
-		for (User u : userCache) {
+		for (User u : cache) {
 			if (u.getId() == id)
 				return u;
 		}
@@ -90,8 +31,8 @@ public class UserRepository extends GsonCreator {
 	
 	public User createNewUser(String name, LocalDate birthDay) {
 		User u = new User(name, birthDay);
-		userCache.add(u);
-		persistUser(u);
+		cache.add(u);
+		persist(u);
 		return u;
 	}
 	
