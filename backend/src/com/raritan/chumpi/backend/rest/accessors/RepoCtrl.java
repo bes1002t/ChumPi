f727d9515ef095b9dfe37a1aaee76e55200f7116
Ben <bes1002t@web.de>
2016-10-29 06:34:01 +0200

    Implemented the Rest API for repository statistics

--- backend/src/com/raritan/chumpi/backend/rest/accessors/RepoCtrl.java
+++ backend/src/com/raritan/chumpi/backend/rest/accessors/RepoCtrl.java
@@ -1,5 +1,34 @@
 package com.raritan.chumpi.backend.rest.accessors;
 
+import java.net.URI;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import com.raritan.chumpi.backend.data.CommitInfo;
+
+@Path("/repo")
 public class RepoCtrl {
 
+	private URI baseUri;
+
+	@GET
+	@Path("/last-commit")
+	public CommitInfo getLastCommit() {
+		return null;
+	}
+
+	@GET
+	@Path("/find")
+	public List<CommitInfo> findCommits(@QueryParam("dir") String dir, @QueryParam("count") int count) {
+		return null;
+	}
+
+	@GET
+	@Path("/last-commits")
+	public List<CommitInfo> getLastCommits(@QueryParam("count") int count) {
+		return null;
+	}
 }
