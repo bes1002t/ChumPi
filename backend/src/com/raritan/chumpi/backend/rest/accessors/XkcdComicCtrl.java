3bb0669a4d9b5cc816fa9e232d1978132330816a
Christian Krause <chkr@plauener.de>
2016-10-29 03:13:36 -0400

    extract image URL for xkcd

--- backend/src/com/raritan/chumpi/backend/rest/accessors/XkcdComicCtrl.java
+++ backend/src/com/raritan/chumpi/backend/rest/accessors/XkcdComicCtrl.java
@@ -1,33 +1,43 @@
 package com.raritan.chumpi.backend.rest.accessors;
 
+import java.io.IOException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
-import org.glassfish.jersey.server.Uri;
-
-import com.raritan.chumpi.backend.data.Image;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 @Path("/xkcd")
 public class XkcdComicCtrl {
-
-	private Uri baseUri;
-
 	@GET
 	@Path("/random")
-	public Image getRandom() {
+	public String getRandom() {
 		return null;
 	}
 
 	@GET
 	@Path("/latest")
-	public Image getLatest() {
+	public String getLatest() {
+		try {
+			Document html = Jsoup.connect("http://www.xkcd.com").get();
+			Element comicdiv = html.select("#comic").first();
+			Element a = comicdiv.child(0);
+			String u = "http:" + a.attr("src");
+			return u;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@GET
 	@Path("/image")
-	public Image getImage(@QueryParam("date") String date) {
+	public String getImage(@QueryParam("date") String date) {
 		return null;
 	}
 }
