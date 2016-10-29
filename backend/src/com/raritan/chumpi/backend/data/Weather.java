95774bd778a6988ac4ac5fb1745db36fa0564698
feesu <fes10031@fh-zwickau.de>
2016-10-29 07:47:04 +0200

    adds weather rest controler. json parse has to be fixed, WeatherProvider creates an uninitialized weather object

--- backend/src/com/raritan/chumpi/backend/data/Weather.java
+++ backend/src/com/raritan/chumpi/backend/data/Weather.java
@@ -2,4 +2,32 @@ package com.raritan.chumpi.backend.data;
 
 public class Weather {
 
+	private double temp;
+	private double pressure;
+	private int humidity;
+
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+
+	public void setPressure(double pressure) {
+		this.pressure = pressure;
+	}
+
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
+
+	public double getTemp() {
+		return temp;
+	}
+
+	public double getPressure() {
+		return pressure;
+	}
+
+	public int getHumidity() {
+		return humidity;
+	}
+
 }
