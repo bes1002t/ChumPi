2c3e6d55c1e63c4318aa3a8edaf6e3738e0078b7
Ben <bes1002t@web.de>
2016-10-29 06:27:47 +0200

    Implemented the REST Api for coffee statistics

--- backend/src/com/raritan/chumpi/backend/rest/accessors/CoffeeStatsCtrl.java
+++ backend/src/com/raritan/chumpi/backend/rest/accessors/CoffeeStatsCtrl.java
@@ -1,5 +1,17 @@
 package com.raritan.chumpi.backend.rest.accessors;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import com.raritan.chumpi.backend.data.provider.CoffeeStatistic;
+
+@Path("/coffee")
 public class CoffeeStatsCtrl {
 
+	@GET
+	@Path("/stat")
+	public CoffeeStatistic getAmountForUser(@QueryParam("userId")  int userId, @QueryParam("from") String from, @QueryParam("to") String to) {
+		return null;
+	}
 }
