3dae878f62e994f8e07b5ea81c62fd233609ebee
Ben <bes1002t@web.de>
2016-10-29 06:33:07 +0200

    Implemented the Rest API for weather

--- backend/src/com/raritan/chumpi/backend/rest/accessors/WeatherCtrl.java
+++ backend/src/com/raritan/chumpi/backend/rest/accessors/WeatherCtrl.java
@@ -1,5 +1,21 @@
 package com.raritan.chumpi.backend.rest.accessors;
 
+import java.net.URI;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+import com.raritan.chumpi.backend.data.Weather;
+
+@Path("/weather")
 public class WeatherCtrl {
 
+	private URI baseUri;
+
+	@GET
+	@Path("/get")
+	public Weather getWeather(@QueryParam("location") int location) {
+		return null;
+	}
 }
